8d734ca3609cede83d3cab90f2e2a6536ff893c4
Vadim Chepil <vadimvamtf@gmail.com>
2024-03-10 16:28:13 +0200

    Created and compiled static library ModulesChepil

---
+++ lab08/prj/ModulesChepil/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void s_calculation(int x, int y, int z)
+{
+    system("chcp 65001 > nul");
+    const double PI = 3.141592653589793238;
+    float S;
+    S = pow(2 * z + 1, x) - sqrt(abs(y - (1 / 2) * z)) + z + PI;
+    cout << "S: " << setprecision(1) << fixed << S << endl;
+}
+
